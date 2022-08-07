bbe77e736c8dda1142106afbd96c248ae62f62f4
Elena <lelya2402@rambler.ru>
2022-08-07 20:24:43 +0300

    Ex010

---
+++ lecture2_CSharp/Ex010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿//
+int[] array = {1,12,31,4,18,16,17,18};
+
+int n=array.Length;
+int find=18;
+
+int index=0;
+
+while (index<n)
+{
+if (array[index]==find)
+{
+    Console.WriteLine(index);
+    break; //прервает цикл при нахождении первого элемента=18
+}
+    //index=index+1;
+    index++;
+}
